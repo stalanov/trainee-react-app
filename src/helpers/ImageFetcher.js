b2407b40a9888ea528f6067c56a9a1ac288986d2
Sergey Talanov <sergey_talanov@lineate.com>
2020-02-26 17:04:14 +0400

    move params const from function in ImageFetcher

--- src/helpers/ImageFetcher.js
+++ src/helpers/ImageFetcher.js
@@ -1,16 +1,15 @@
+const params = {
+  action: 'query',
+  formatversion: 2,
+  prop: 'pageimages',
+  piprop: 'original',
+  pilicense: 'any',
+  format: 'json',
+  redirects: 1
+};
+
 export default async function getImageUrl(title) {
   let url = 'https://en.wikipedia.org/w/api.php?origin=*';
-
-  const params = {
-    action: 'query',
-    formatversion: 2,
-    prop: 'pageimages',
-    piprop: 'original',
-    pilicense: 'any',
-    format: 'json',
-    redirects: 1
-  };
-
   Object.keys(params).forEach(function(key) {
     url += '&' + key + '=' + params[key];
   });
